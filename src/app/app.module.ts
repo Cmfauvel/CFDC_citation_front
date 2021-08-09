693685eff3fdb622cc3994837101b58162c8ceb8
Charles-Chrismann <charles.chrismann@free.fr>
2021-08-09 14:45:00 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  loginForm?: FormGroup;
+  
+  constructor(
+    private router : Router,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.initConnexionForm();
+  }
+
+  initConnexionForm(): void {
+    this.loginForm = this.fb.group({
+      mail: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      pseudo: this.fb.control('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    const user = {
+      email: this.loginForm.value.mail,
+      username: this.loginForm.value.pseudo,
+      password: this.loginForm.value.password,
+    }
+    console.log(user);
+    // this.authService.login(user).subscribe(
+    //   (resp: any) => {
+    //     console.log("Connection succeed", resp);
+    //     this.router.navigate(['/']);
+    //   },
+    //   error => {
+    //     console.log('error while');
+    //   }
+
+    // )
+  }
+}
