d83a9ed0ea49e6a703dac02e33927c7a71406f60
Charles-Chrismann <charles.chrismann@free.fr>
2021-08-09 15:01:36 +0200

    register et login, manque AuthService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+//import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ export class LoginComponent implements OnInit {
   loginForm?: FormGroup;
   
   constructor(
+    //private auth: AuthService,
     private router : Router,
     private fb: FormBuilder
   ) { }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(//private auth: AuthService,
+    private fb: FormBuilder) { 
+      // this.registerForm = new FormGroup({});
+    }
 
   ngOnInit(): void {
+    this.initRegisterForm();
   }
+
+  initRegisterForm(): void {
+    this.registerForm = this.fb.group({
+      pseudo: this.fb.control('', [Validators.required]),
+      mail: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirmPassword: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(8),
+      ])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.registerForm.value)
+    const user = {
+      username: this.registerForm.value.pseudo,
+      email: this.registerForm.value.mail,
+      role: ["USER"],
+      password: this.registerForm.value.password
+    }
+    console.log(user)
+
+    //this.auth.register(user).subscribe((response) => console.log(response))
+  }
+
+  
 
 }
